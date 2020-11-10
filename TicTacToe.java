cdf9122c576c203f5de614a9724adfe7ce3aab77
Yash <yashn49@gmail.com>
2020-11-10 16:38:37 +0530

    third commit

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,13 +1,13 @@
 import java.util.*;
 import java.util.Scanner;
 
-
 public class TicTacToe
 {
  public static void main(String[] args)
  {
   System.out.println("Welcome to tictactoe game");
   createBoard();
+  chooseLetter();
  }
  public static void createBoard()
  {
@@ -15,6 +15,20 @@ public class TicTacToe
   for(int i=0; i<10; i++)
   {
    board[0]=' ';
+  }
+ }
+ public static void chooseLetter()
+ {
+  System.out.println("enter your choice. Press 1 for X or Press 2 for O");
+  Scanner a=new Scanner(System.in);
+  int chosenLetter = a.nextInt();
+  if(chosenLetter==1)
+  {
+   System.out.println("you have chosen X");
+  }
+  else
+  {
+  System.out.println("you have chosen O");
   }
  }
 }
