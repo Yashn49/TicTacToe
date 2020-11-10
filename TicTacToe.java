14552b75075377aeb8f55c910ec0d149ae674334
Yash <yashn49@gmail.com>
2020-11-10 16:22:50 +0530

    second commit

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,8 +1,20 @@
 import java.util.*;
+import java.util.Scanner;
+
+
 public class TicTacToe
 {
  public static void main(String[] args)
  {
   System.out.println("Welcome to tictactoe game");
+  createBoard();
+ }
+ public static void createBoard()
+ {
+  char board[] = new char[10];
+  for(int i=0; i<10; i++)
+  {
+   board[0]=' ';
+  }
  }
 }
